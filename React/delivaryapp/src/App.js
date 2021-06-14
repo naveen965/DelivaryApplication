712ecc633d9b4b32d23b17ac09874791870de647
naveen965 <naveendevinda163@gmail.com>
2021-06-14 21:29:51 +0530

    Edited

--- React/delivaryapp/src/App.js
+++ React/delivaryapp/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Booom Play
         </a>
       </header>
     </div>
