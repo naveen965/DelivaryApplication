3fe10c31e17c889f767cbca11a447311b6fbf25e
naveen965 <naveendevinda163@gmail.com>
2021-06-19 20:18:51 +0530

    Edited

--- React/delivaryapp/src/App.js
+++ React/delivaryapp/src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Booom Play
-        </a>
       </header>
     </div>
   );
